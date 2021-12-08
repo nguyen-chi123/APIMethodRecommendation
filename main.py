2f7a1fc12921069d89074edd5b5e83056339b74f
Nguyen Chi <nguyenninhchi123@gmail.com>
2021-12-08 09:12:26 +0700

    refactor

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ FUNC_VERB_NET = FuncVerbNet()
 
 
 class PreMA:
-    def __init__(self, sentence):
-        self.f_sentence = sentence
-        self.f_category = self.find_f_category()
-        self.f_verb = self.find_f_verb()
+    def __init__(self, f_sentence):
+        self._f_sentence = sentence
+        self._f_category = self.find_f_category()
+        self._f_verb, self._vector_doc = self.find_f_verb()
 
     def find_f_category(self):
         """
@@ -16,35 +16,49 @@ class PreMA:
         :param sentence:
         :return: category
         """
-        category_id = FUNC_VERB_NET.find_category_by_any_sentence(self.f_sentence)
+        category_id = FUNC_VERB_NET.find_category_by_any_sentence(self._f_sentence)
         cate = FUNC_VERB_NET.find_cate_by_id(category_id)
         return cate
 
     def find_f_verb(self):
+        if self._f_category.id == -1:
+            return None
         nlp = spacy.load("en_core_web_sm")
-        doc = nlp(self.f_sentence)
+        doc = nlp(self._f_sentence)
         f_verb = None
-        include_verb = self.f_category.included_verb
+        include_verb = self._f_category.included_verb
         for token in doc:
             if token.tag_ == 'VB' and token.text in include_verb:
                 f_verb = token.text
                 break
-            if token.tag_ == 'VB':
-                f_verb = token.text
-                break
-        return f_verb
+        # khi ko tìm thấy verb thuộc f_category, TH này chưa gặp ??
+        if f_verb is None:
+            for token in doc:
+                if token.tag_ == 'VB':
+                    f_verb = token.text
+                    break
+        return f_verb, doc
 
-    # def find_p_pattern(self):
+    def find_p_pattern(self):
+        for token in self._vector_doc:
+            if token.tag_ != 'VB':
+                continue
+            # ?? đệ quy chuyển đổi sang cấu trúc SP
 
 
 if __name__ == "__main__":
-    sentence = 'use this method to get the default sensor for a given type'
+    sentence = 'Attempt to compute for the specified key and its current mapped value \
+                (or null if there is no current mapping)'
     finder = PreMA(sentence)
 
     # 4.1 phân loại f_sentence vào f_category
-    print(finder.f_category.id, finder.f_category.name)
+    print('phân loại vào f_category: ', finder.f_category.name)
     # 4.2 phân tích p_pattern
-    print('________________________________________________________________')
+    print('Phân tích p_pattern')
+    print('xác định f_verb: ', finder.f_verb)
+
+
+
 
 
 
